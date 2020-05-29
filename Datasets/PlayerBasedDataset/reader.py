825a6a946263ef8a3960e8f5b9bb72b38d259840
Svitlana Doroshenko <s.lana.doroshenko@gmail.com>
2020-05-29 21:58:43 +0300

    Added columns to be used, optimized names parsing.

--- Datasets/PlayerBasedDataset/reader.py
+++ Datasets/PlayerBasedDataset/reader.py
@@ -4,9 +4,13 @@ import numpy as np
 
 class PlayerBasedDataReader:
 
-    def __init__(self, filename: str):
+    def __init__(self, filename: str, feature_cols: list, y_cols):
         self.raw_data = None
         self.valide_data = None
+        self.feature_cols = feature_cols
+        self.y_cols = y_cols
+        self.used_cols = feature_cols + y_cols
+
         self.read_dataset(filename)
         self.preprocess_data()
 
@@ -17,24 +21,42 @@ class PlayerBasedDataReader:
     def preprocess_data(self):
         new_data = self.raw_data.query('dire_score !=0 & radiant_score!=0')
         new_data = new_data.query('duration >899')
+
         new_data = new_data.sort_values(by=['start_time'])
+        new_data = new_data[self.used_cols].dropna()
+
         data_size = new_data.shape[0]
         for i in range(1, 6):
-            new_data['dire_player_' + str(i)] = np.zeros(data_size)
-            new_data['radiant_player_' + str(i)] = np.zeros(data_size)
-        for i in range(data_size):
-            for j in range(5):
-                new_data.at[i, 'radiant_player_'+str(j+1)] = new_data.iloc[i]['players_radiant_id'][j]
-                new_data.at[i, 'dire_player_'+str(j+1)] = new_data.iloc[i]['players_dire_id'][j]
+            new_data[f'dire_player_{i}'] = np.zeros(data_size)
+            new_data[f'radiant_player_{i}'] = np.zeros(data_size)
+
+        idx_r = new_data.columns.get_loc('players_radiant_id')
+        idx_d = new_data.columns.get_loc('players_dire_id')
+
+        players_radiant_id = np.asarray(new_data.iloc[:, idx_r].values.tolist())
+        players_dire_id = np.asarray(new_data.iloc[:, idx_d].values.tolist())
+        print(players_radiant_id.shape, players_radiant_id.dtype)
+
+        for j in range(5):
+            new_data.loc[:, f'radiant_player_{j + 1}'] = players_radiant_id[:, j]
+            new_data.loc[:, f'dire_player_{j + 1}'] = players_dire_id[:, j]
+
         new_data = new_data.drop(columns=['players_radiant_id', 'players_dire_id'])
+
         self.valide_data = new_data
 
     def write_data(self, filename: str):
         with open(filename, 'wb') as file:
             pk.dump(self.valide_data, file)
 
+    def find_unique_players(self):
+        pass
+
 
 if __name__ == '__main__':
-    data = PlayerBasedDataReader('../BaseDataset/dota2_dataset.pickle')
-    data.write_data('PlayerBasedData.pickle')
-    print(data.valide_data['radiant_player_1'])
+    feature_cols_ = ['dire_score', 'radiant_score', 'duration', 'patch', 'region', 'radiant_team_id', 'dire_team_id',
+                     'players_radiant_id', 'players_dire_id']
+    y_cols_ = ['radiant_win']
+    data = PlayerBasedDataReader('../BaseDataset/dota2_dataset.pickle', feature_cols_, y_cols_)
+    # data.write_data('PlayerBasedData.pickle')
+    # print(data.valide_data['radiant_player_1'])
