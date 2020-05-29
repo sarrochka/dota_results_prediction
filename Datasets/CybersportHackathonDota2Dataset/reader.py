8023469683cf9f07370a1ab675e5225bc8c4b61b
Nikita <nikitaulyankinipad@gmail.com>
2020-05-29 15:20:03 +0300

    added DataReader

--- Datasets/CybersportHackathonDota2Dataset/reader.py
+++ Datasets/CybersportHackathonDota2Dataset/reader.py
@@ -1,8 +1,29 @@
 import pickle as pk
-import numpy as np
 
-file = open("validated_data.pickle","rb")
-data = pk.load(file)
-file.close()
 
-print (data)
+class DataReader:
+
+    def __init__(self, filename: str):
+        self.raw_data = None
+        self.valide_data = None
+        self.read_dataset(filename)
+        self.validate_data()
+
+    def read_dataset(self, filename: str):
+        with open(filename, 'rb') as file:
+            self.raw_data = pk.load(file)
+
+    def validate_data(self):
+        new_data = self.raw_data.query('dire_score !=0 & radiant_score!=0')
+        new_data = new_data.query('duration >899')
+        self.valide_data = new_data
+
+    def write_data(self, filename: str):
+        with open(filename, 'wb') as file:
+            pk.dump(self.valide_data, file)
+
+
+if __name__ == '__main__':
+    reader = DataReader('dota2_dataset.pickle')
+    reader.write_data('validated_data.pickle')
+    print(reader.valide_data)
