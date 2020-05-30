578981cacb1991185b2f709f7dbda284254910b1
Svitlana Doroshenko <s.lana.doroshenko@gmail.com>
2020-05-30 18:35:45 +0300

    Added model saving.

--- model/nn.py
+++ model/nn.py
@@ -7,7 +7,7 @@ from Datasets.BaseDataset.reader import DataReader
 
 class ResultPredictingModel:
     def __init__(self, y, h_list, cost_weigths):
-        super().__init__(y)
+        self.y = y
         self.h_list = h_list
         self.cost_weights = cost_weigths
         self.n_y = y.shape[1]
@@ -71,7 +71,7 @@ if __name__ == '__main__':
     x_cols = ['avg_dire_score', 'avg_radiant_score', 'avg_duration', 'patch', 'region']
 
     # if they are used as input
-    # x_cols += ['radiant_team_id', 'dire_team_id']
+    x_cols += ['radiant_team_id', 'dire_team_id']
     # x_cols += [f'radiant_player_{j}' for j in range (1, 6)] + [f'dire_player_{j}' for j in range(1, 6)]
 
     data_reader = DataReader('Datasets/BaseDataset/dota2_dataset.pickle', feature_cols, y_cols, x_cols)
@@ -84,11 +84,7 @@ if __name__ == '__main__':
     n_x = train_x.shape[1]
     n_y = train_y.shape[1]
 
-    tf.reset_default_graph()
-
     lr = 1e-3
-
-    g = tf.Graph()
 
     radiant_wr = np.where(data_reader.preprocessed_data[y_cols])[0].shape[0] / \
                  data_reader.preprocessed_data[y_cols].shape[0]
@@ -96,55 +92,54 @@ if __name__ == '__main__':
     print(radiant_wr)
     cost_weigths = np.asarray([radiant_wr, 1.-radiant_wr])
 
-    with g.as_default():
-        x = tf.placeholder(tf.float32, shape=[None, n_x, 1], name="x")
-        y = tf.placeholder(tf.float32, shape=[None, n_y], name="y")
-        tf.constant(cost_weigths)
-        saver = tf.train.Saver()
+    x = tf.placeholder(tf.float32, shape=[None, n_x, 1], name="x")
+    y = tf.placeholder(tf.float32, shape=[None, n_y], name="y")
+    tf.constant(cost_weigths)
 
     h = [500, 100, 50]
 
     model = ResultPredictingModel(y=y, h_list=h, cost_weigths=cost_weigths)
-
-    with g.as_default():
-        model.initialize()
-
-    sess = tf.InteractiveSession(graph=g)
-    init = tf.global_variables_initializer()
-    sess.run(init)
+    model.initialize()
 
     num_epochs = 250
+    init_op = tf.global_variables_initializer()
 
-    for epoch in range(num_epochs):
-        if epoch % 10 == 0:
-            print(epoch)
-        _, c = sess.run([model.optimize(), model.cost()], feed_dict={x: np.expand_dims(train_x, axis=-1), y: train_y})
+    saver = tf.train.Saver()
 
-    save_path = saver.save(sess, "/model.ckpt")
+    with tf.Session() as sess:
+        sess.run(init_op)
 
-    print('Accuracy val: ', model.accuracy().eval({x: np.expand_dims(val_x, axis=-1), y: val_y}))
-    prediction = model.prediction().eval({x:  np.expand_dims(val_x, axis=-1), y: val_y})
+        for epoch in range(num_epochs):
+            if epoch % 10 == 0:
+                print(epoch)
+            _, c = sess.run([model.optimize(), model.cost()], feed_dict={x: np.expand_dims(train_x, axis=-1),
+                                                                         y: train_y})
 
-    print('Prediction: ', prediction)
+        save_path = saver.save(sess, "model/model.ckpt")
+        acc_val = sess.run(model.accuracy(), feed_dict={x: np.expand_dims(val_x, axis=-1), y: val_y})
+        pred_val = sess.run(model.prediction(), feed_dict={x: np.expand_dims(val_x, axis=-1), y: val_y})
 
-    with open('train_x.pickle', 'wb') as file:
+    print('Accuracy val: ', acc_val)
+    print('Prediction: ', pred_val)
+
+    with open('Datasets/BaseDataset/train_val_test/train_x.pickle', 'wb') as file:
         pk.dump(train_x, file)
 
-    with open('train_y.pickle', 'wb') as file:
+    with open('Datasets/BaseDataset/train_val_test/train_y.pickle', 'wb') as file:
         pk.dump(train_y, file)
 
-    with open('val_x.pickle', 'wb') as file:
+    with open('Datasets/BaseDataset/train_val_test/val_x.pickle', 'wb') as file:
         pk.dump(train_x, file)
 
-    with open('val_y.pickle', 'wb') as file:
+    with open('Datasets/BaseDataset/train_val_test/val_y.pickle', 'wb') as file:
         pk.dump(train_y, file)
 
-    with open('test_x.pickle', 'wb') as file:
+    with open('Datasets/BaseDataset/train_val_test/test_x.pickle', 'wb') as file:
         pk.dump(train_x, file)
 
-    with open('test_y.pickle', 'wb') as file:
+    with open('Datasets/BaseDataset/train_val_test/test_y.pickle', 'wb') as file:
         pk.dump(train_y, file)
 
-    with open('prediction.pickle', 'wb') as file:
-        pk.dump(prediction, file)
+    with open('Datasets/BaseDataset/train_val_test/prediction_val.pickle', 'wb') as file:
+        pk.dump(pred_val, file)
 
